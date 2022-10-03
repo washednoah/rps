d2f795e5ababa19ac0a15be587b1e62ed950ed86
washednoah <114591615+washednoah@users.noreply.github.com>
2022-10-03 08:56:43 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,130 @@
+input.onButtonPressed(Button.A, function () {
+    if (state == 0) {
+        hand += 1
+        if (hand > 3) {
+            hand = 1
+        }
+        if (hand == 1) {
+            basic.showLeds(`
+                . . . . .
+                . # # # .
+                . # # # .
+                . # # # .
+                . . . . .
+                `)
+        } else if (hand == 2) {
+            basic.showLeds(`
+                # # # # #
+                # # # # #
+                # # # # #
+                # # # # #
+                # # # # #
+                `)
+        } else if (hand == 3) {
+            basic.showLeds(`
+                # # . . #
+                # # . # .
+                . . # . .
+                # # . # .
+                # # . . #
+                `)
+        }
+    } else {
+        hand_2 += 1
+        if (hand_2 == 1) {
+            basic.showLeds(`
+                . . . . .
+                . # # # .
+                . # # # .
+                . # # # .
+                . . . . .
+                `)
+        } else if (hand_2 == 2) {
+            basic.showLeds(`
+                # # # # #
+                # # # # #
+                # # # # #
+                # # # # #
+                # # # # #
+                `)
+        } else if (hand_2 == 3) {
+            basic.showLeds(`
+                # # . . #
+                # # . # .
+                . . # . .
+                # # . # .
+                # # . . #
+                `)
+        }
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    if (state == 0) {
+        state += 1
+        basic.showString("PLayer 2")
+    } else {
+    	
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (state == 0) {
+        hand += -1
+        if (hand < 3) {
+            hand = 1
+            basic.showLeds(`
+                . . . . .
+                . # # # .
+                . # # # .
+                . # # # .
+                . . . . .
+                `)
+        } else if (hand == 2) {
+            basic.showLeds(`
+                # # # # #
+                # # # # #
+                # # # # #
+                # # # # #
+                # # # # #
+                `)
+        } else if (hand == 3) {
+            basic.showLeds(`
+                # # . . #
+                # # . # .
+                . . # . .
+                # # . # .
+                # # . . #
+                `)
+        }
+    } else {
+        hand_2 += -1
+        if (hand_2 == 1) {
+            basic.showLeds(`
+                . . . . .
+                . # # # .
+                . # # # .
+                . # # # .
+                . . . . .
+                `)
+        } else if (hand_2 == 2) {
+            basic.showLeds(`
+                # # # # #
+                # # # # #
+                # # # # #
+                # # # # #
+                # # # # #
+                `)
+        } else if (hand_2 == 3) {
+            basic.showLeds(`
+                # # . . #
+                # # . # .
+                . . # . .
+                # # . # .
+                # # . . #
+                `)
+        }
+    }
+})
+let hand_2 = 0
+let hand = 0
+let state = 0
+state = 0
